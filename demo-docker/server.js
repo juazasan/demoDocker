3073772cd8655ab4a3c8e6272f86c7cd92306a4b
juazasan <juazasan@msn.com>
2016-09-05 08:12:07 +0200

    first release

---
+++ demo-docker/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+const os = require('os'); 
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    var hostname = os.hostname();
+    var returnMessage = 'Hello World from server:\n';
+    returnMessage += os.hostname() + '\n';
+    returnMessage += 'Network Interfaces:\n';    
+    returnMessage +=  JSON.stringify(os.networkInterfaces(), null, 2)+ '\n';    
+    response.end(returnMessage);
+}).listen(80);
+
+console.log('Server started');
