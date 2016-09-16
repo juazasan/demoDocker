a7325ccb8d4c8d66c02500674f105aa5e19b5521
juazasan <juazasan@msn.com>
2016-09-16 17:40:53 +0200

    pruebas nginx

--- demo-docker/server.js
+++ demo-docker/server.js
@@ -3,8 +3,8 @@ const os = require('os');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     var hostname = os.hostname();
-    var returnMessage = 'Hello World from server:\n';
-    returnMessage += os.hostname() + '\n';
+    var returnMessage = 'Hello World!!!\n';
+    returnMessage += 'server: '+ os.hostname() +'\n';
     returnMessage += 'Network Interfaces:\n';    
     returnMessage +=  JSON.stringify(os.networkInterfaces(), null, 2)+ '\n';    
     response.end(returnMessage);
